3664f5304eb67652dc57d9414a0b149a19441e1a
Calvin Luong <51200417+xKleinx@users.noreply.github.com>
2022-12-01 13:19:05 -0500

    Update streamlit_app.py
    Normalized the json
    added a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# normalize the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output as a table
+streamlit.dataframe(fruityvice_normalized)
