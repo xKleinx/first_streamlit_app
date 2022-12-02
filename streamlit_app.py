19c1559dca11d177912ff7408cf8302e993e0397
Calvin Luong <51200417+xKleinx@users.noreply.github.com>
2022-12-01 20:48:09 -0500

    Update streamlit_app.py
    Added section to test query to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# test snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
