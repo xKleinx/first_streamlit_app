53b60358c0c9984d1438d7bd4bb23c1178595b6c
Calvin Luong <51200417+xKleinx@users.noreply.github.com>
2022-12-01 21:04:03 -0500

    Update streamlit_app.py
    removed the code that pre-loaded Kiwi into the field

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,11 +25,11 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # normalize the json
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # output as a table
